f00c799f7eb95c949e1ee0db1c36c860ab502319
Vu Cu <cxv@g.ucla.edu>
2017-11-22 09:36:57 +0700

    new customer api

--- TableExportWebApi/Controllers/NewCustomerController.cs
+++ TableExportWebApi/Controllers/NewCustomerController.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using TableExportWebApi.Models;
+
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TableExportWebApi.Controllers
 {
-    [Route("api/[controller]")]
-    public class Standard : Controller
+    [Route("api/new-customer")]
+    public class NewCustomerController : Controller
     {
-        // GET: api/standard
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private long generateUniqueId()
         {
-            return new string[] { "This is API root" };
+            return DateTime.Now.ToFileTime();
+        }
+
+        // GET: api/root
+        [HttpGet]
+        public long Get()
+        {
+            return generateUniqueId();
         }
     }
 }
